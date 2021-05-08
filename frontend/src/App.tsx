ba2237b7385e1ae7e9077716566675642e40bd73
Pedro Rocha <45208073+pedroanastacio@users.noreply.github.com>
2021-05-07 21:26:59 -0300

    BarChart integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import BarChart from "components/barChart";
-import DataTable from "components/dataTable";
-import DonutChart from "components/donutChart";
-import Footer from "components/footer";
-import NavBar from "components/navbar";
 import Routes from "routes";
 
 function App() {
--- frontend/src/pages/notFound/index.tsx
+++ frontend/src/pages/notFound/index.tsx
@@ -1,6 +1,5 @@
 import Footer from "components/footer";
 import NavBar from "components/navbar";
-import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
